c4af8104237c832d92b1e8833362b95c601f6469
CarlosSalgadoFernandez <carlossalgado.mail@gmail.com>
2024-07-13 18:15:03 +0200

    v0.0.2

--- intropython.py
+++ intropython.py
@@ -1,1 +1,17 @@
 print("hola mundo")
+print("ejercicios")
+print("1 Minimo dos números a y b")
+a=2
+b=4
+def minimo(a,b):
+    print("El número menor es",min(a,b))
+minimo(a,b)
+
+print("2 revertir una cadena ")
+cadena=input("escribe un texto: ")
+def alreves(texto):
+    letras = texto.split(' ')
+    otrotexto = ' '.join(reversed(letras))
+    print(otrotexto)
+
+alreves(cadena)
